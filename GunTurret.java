28383e16db9afe83af336c01929ad9358af5e6bf
matheus051022 <matheus.bionicle@gmail.com>
2016-04-23 02:02:43 -0300

    Fix GunTurret

--- GunTurret.java
+++ GunTurret.java
@@ -1,44 +1,21 @@
+/*
+ *	This class is part of BattleTank 2.
+ *
+ *  BattleTank 2 is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  BattleTank 2 is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with BattleTank 2.  If not, see <http://www.gnu.org/licenses/>
+ */
+
 public class GunTurret extends SolidObject {
-	private Polygon3D body[];
-
-	private Polygon3D turret[];
-
-	private Polygon3D shadowBody;
-
-	private Polygon3D shadowTurret;
-
-	private Vector bodyCenter;
-
-	private Vector turretCenter;
-
-	private int turretAngle;
-
-	private int targetAngle;
-
-	private int turretAngleDelta;
-
-	private boolean aimRight, aimLeft, firing;
-
-	private int coolDown = 20;
-
-	private int currentCoolDown = 0;
-
-	private int position;
-
-	private Smoke Smoke;
-
-	private boolean isVisiblePreviousFrame;
-
-	private Vector tempVector1 = new Vector(0, 0, 0);
-	private Vector tempVector2 = new Vector(0, 0, 0);
-
-	private boolean engaged;
-
-	private double distance;
-
-	private boolean clearToShoot;
-
-	private boolean destoried;
 
 	public GunTurret(double x, double y, double z, int angle) {
 
@@ -81,135 +58,6 @@ public class GunTurret extends SolidObject {
 		HP = 75;
 
 		lifeSpan = 1;
-	}
-
-	private void makeBody() {
-		Vector v[];
-
-		body = new Polygon3D[5];
-
-		v = new Vector[] { put(-0.09, 0.09, -0.09), put(0.09, 0.09, -0.09),
-				put(0.12, 0, -0.12), put(-0.12, 0, -0.12) };
-		body[0] = new Polygon3D(v, v[0], v[1], v[3], Main.textures[41], 1, 1, 6);
-
-		v = new Vector[] { put(-0.09, 0.09, 0.09), put(-0.09, 0.09, -0.09),
-				put(-0.12, 0, -0.12), put(-0.12, 0, 0.12) };
-		body[1] = new Polygon3D(v, v[0], v[1], v[3], Main.textures[41], 1, 1, 6);
-
-		v = new Vector[] { put(0.12, 0, 0.12), put(0.12, 0, -0.12),
-				put(0.09, 0.09, -0.09), put(0.09, 0.09, 0.09) };
-		body[2] = new Polygon3D(v, v[0], v[1], v[3], Main.textures[41], 1, 1, 6);
-
-		v = new Vector[] { put(-0.12, 0, 0.12), put(0.12, 0, 0.12),
-				put(0.09, 0.09, 0.09), put(-0.09, 0.09, 0.09) };
-		body[3] = new Polygon3D(v, v[0], v[1], v[3], Main.textures[41], 1, 1, 6);
-
-		v = new Vector[] { put(-0.09, 0.09, 0.09), put(0.09, 0.09, 0.09),
-				put(0.09, 0.09, -0.09), put(-0.09, 0.09, -0.09) };
-		body[4] = new Polygon3D(v, v[0], v[1], v[3], Main.textures[41], 1, 1, 6);
-
-		tempVector1 = start.myClone();
-
-		iDirection.scale(1.2);
-		start.add(0, 0, -0.03);
-		v = new Vector[] { put(-0.2, 0, 0.2), put(0.2, 0, 0.2),
-				put(0.2, 0, -0.2), put(-0.2, 0, -0.2) };
-		shadowBody = new Polygon3D(v, v[0], v[1], v[3], Main.textures[50], 1,
-				1, 2);
-
-		for (int i = 0; i < body.length; i++) {
-			body[i].Ambient_I = 22;
-			body[i].diffuse_coefficient = 0.7;
-			body[i].findDiffuse();
-
-		}
-
-		start.set(tempVector1);
-		iDirection.scale(0.8333333333);
-
-	}
-
-	private void makeTurret() {
-		iDirection = new Vector(0.7, 0, 0);
-		jDirection = new Vector(0, 0.7, 0);
-		kDirection = new Vector(0, 0, 0.7);
-
-		iDirection.rotate_XZ(turretAngle);
-		kDirection.rotate_XZ(turretAngle);
-
-		iDirection.rotate_XZ(180);
-		kDirection.rotate_XZ(180);
-
-		turret = new Polygon3D[22];
-
-		Vector v[];
-
-		v = new Vector[] { put(-0.04, 0.16, -0.08), put(0.04, 0.16, -0.08),
-				put(0.04, 0.09, -0.08), put(-0.04, 0.09, -0.08) };
-		turret[0] = new Polygon3D(v, v[0], v[1], v[3], Main.textures[51], 1,
-				0.5, 6);
-
-		v = new Vector[] { put(-0.08, 0.16, 0.08), put(-0.04, 0.16, -0.08),
-				put(-0.04, 0.09, -0.08), put(-0.08, 0.09, 0.08) };
-		turret[1] = new Polygon3D(v, v[0], v[1], v[3], Main.textures[51], 1,
-				0.5, 6);
-
-		v = new Vector[] { put(0.08, 0.16, 0.08), put(-0.08, 0.16, 0.08),
-				put(-0.08, 0.09, 0.08), put(0.08, 0.09, 0.08) };
-		turret[2] = new Polygon3D(v, v[0], v[1], v[3], Main.textures[51], 1,
-				0.5, 6);
-
-		v = new Vector[] { put(0.04, 0.16, -0.08), put(0.08, 0.16, 0.08),
-				put(0.08, 0.09, 0.08), put(0.04, 0.09, -0.08) };
-		turret[3] = new Polygon3D(v, v[0], v[1], v[3], Main.textures[51], 1,
-				0.5, 6);
-
-		double r = 0.016;
-		double r1 = 0.012;
-		double theta = Math.PI / 8;
-		start.add(0, 0.1, 0);
-
-		for (int i = 0; i < 16; i++) {
-			v = new Vector[] {
-					put(r1 * Math.cos(i * theta), r1 * Math.sin(i * theta),
-							-0.22),
-					put(r1 * Math.cos((i + 1) * theta),
-							r1 * Math.sin((i + 1) * theta), -0.22),
-					put(r * Math.cos((i + 1) * theta),
-							r * Math.sin((i + 1) * theta), -0.08),
-					put(r * Math.cos(i * theta), r * Math.sin(i * theta), -0.08) };
-			turret[i + 4] = new Polygon3D(v, v[0], v[1], v[3],
-					Main.textures[23], 0.001, 0.01, 6);
-			turret[i + 4].Ambient_I = 18;
-			turret[i + 4].diffuse_coefficient = 0.7;
-			turret[i + 4].findDiffuse();
-		}
-
-		v = new Vector[16];
-		for (int i = 1; i < 17; i++)
-			v[16 - i] = put(r1 * Math.cos(i * theta), r1 * Math.sin(i * theta),
-					-0.22);
-		turret[21] = new Polygon3D(v, v[0], v[1], v[3], Main.textures[23], 1,
-				1, 6);
-
-		start.add(0, -0.1, 0);
-
-		v = new Vector[] { put(-0.08, 0.16, 0.08), put(0.08, 0.16, 0.08),
-				put(0.04, 0.16, -0.08), put(-0.04, 0.16, -0.08) };
-		turret[20] = new Polygon3D(v, v[0], v[1], put(-0.08, 0.16, -0.08),
-				Main.textures[51], 1, 1, 6);
-
-		start.add(-0.05, 0, -0.05);
-		start.y = -1;
-		v = new Vector[] { put(0.5, 0, -0.27), put(-0.5, 0, -0.27),
-				put(-0.5, 0, 0.27), put(0.5, 0, 0.27) };
-		shadowTurret = new Polygon3D(v, v[0], v[1], v[3], Main.textures[15], 1,
-				1, 2);
-		start.add(0.05, 0, 0.05);
-
-		iDirection.rotate_XZ(180);
-		kDirection.rotate_XZ(180);
-
 	}
 
 	public void update() {
@@ -332,11 +180,16 @@ public class GunTurret extends SolidObject {
 				shadowTurret.update();
 				Rasterizer.rasterize(shadowTurret);
 
+			} else {
+				// Does nothing.
 			}
 		}
 
-		if (currentCoolDown > 0 && !Main.gamePaused)
+		if (currentCoolDown > 0 && !Main.gamePaused) {
 			currentCoolDown--;
+		} else {
+			// Does nothing.
+		}
 
 		if (firing) {
 			if (currentCoolDown == 0) {
@@ -345,10 +198,12 @@ public class GunTurret extends SolidObject {
 				Vector direction = new Vector(0, 0, 1);
 				direction.rotate_XZ(turretAngle);
 				direction.scale(0.06);
-				Projectiles
-						.register(new Shell(centre.x + direction.x, centre.y,
-								centre.z + direction.z, turretAngle, true, 1));
+				Projectiles.register(new Shell(centre.x + direction.x, centre.y,centre.z + direction.z, turretAngle, true, 1));
+			} else {
+				// Does nothing.
 			}
+		} else {
+			// Does nothing.
 		}
 
 		if (HP <= 37) {
@@ -357,8 +212,11 @@ public class GunTurret extends SolidObject {
 				tempVector1.y += 0.1;
 				Smoke = new Smoke(tempVector1.myClone());
 			} else {
-				if (visible)
+				if (visible) {
 					Smoke.update();
+				} else {
+					// Does nothing.
+				}
 			}
 		}
 
@@ -368,7 +226,10 @@ public class GunTurret extends SolidObject {
 						centre.z, 2));
 				modelType = 6;
 				Smoke.stopped = true;
+			} else {
+				// Does nothing.
 			}
+				
 			destoried = true;
 		}
 
@@ -379,21 +240,94 @@ public class GunTurret extends SolidObject {
 
 	}
 
+	public void draw() {
+
+		for (int i = 0; i < body.length; i++) {
+			body[i].draw();
+		}
+
+		if (HP > 0) {
+			for (int i = 0; i < turret.length; i++) {
+				turret[i].draw();
+			}
+		} else {
+			// Does nothing.
+		}
+
+		if (Smoke != null && visible) {
+			Smoke.draw();
+		}
+	}
+
+	public Rectangle2D getBoundary2D() {
+		return boundary2D;
+	}
+
+	public void damage(int damagePoint) {
+		HP -= damagePoint;
+	}
+	
+	private Polygon3D body[];
+
+	private Polygon3D turret[];
+
+	private Polygon3D shadowBody;
+
+	private Polygon3D shadowTurret;
+
+	private Vector bodyCenter;
+
+	private Vector turretCenter;
+
+	private int turretAngle;
+
+	private int targetAngle;
+
+	private int turretAngleDelta;
+
+	private boolean aimRight, aimLeft, firing;
+
+	private int coolDown = 20;
+
+	private int currentCoolDown = 0;
+
+	private int position;
+
+	private Smoke Smoke;
+
+	private boolean isVisiblePreviousFrame;
+
+	private Vector tempVector1 = new Vector(0, 0, 0);
+	
+	private Vector tempVector2 = new Vector(0, 0, 0);
+
+	private boolean engaged;
+
+	private double distance;
+
+	private boolean clearToShoot;
+
+	private boolean destoried;
+	
 	private void processAI() {
 
 		tempVector1.set(centre);
 		tempVector1.subtract(PlayerTank.bodyCenter);
 		distance = tempVector1.getLength();
 
-		if (distance < 2.3)
+		if (distance < 2.3) {
 			engaged = true;
-		else
+		} else {
 			engaged = false;
-
+		}
+		
 		if (engaged) {
-			if ((Main.timer) % 5 == 0)
+			if ((Main.timer) % 5 == 0) {
 				ObstacleMap.alertNearbyTanks(position);
-
+			} else {
+				// Does nothing.
+			}
+			
 			tempVector1.set(bodyCenter);
 			tempVector2.set(PlayerTank.bodyCenter);
 			tempVector2.subtract(tempVector1);
@@ -406,8 +340,12 @@ public class GunTurret extends SolidObject {
 			for (int i = 0; (d < distance) && (i < 35); i++, tempVector1
 					.add(tempVector2), d += 0.125) {
 				model temp = ObstacleMap.isOccupied2(tempVector1);
-				if (temp == null)
+				if (temp == null) {
 					continue;
+				} else {
+					// Does nothing
+				}
+				
 				obstacleType = temp.getType();
 				if (obstacleType == 1) {
 					break;
@@ -423,53 +361,169 @@ public class GunTurret extends SolidObject {
 						.atan((centre.z - PlayerTank.bodyCenter.z)
 								/ (centre.x - PlayerTank.bodyCenter.x)) / Math.PI);
 				if (PlayerTank.bodyCenter.x > turretCenter.x
-						&& targetAngle <= 180)
+						&& targetAngle <= 180) {
 					targetAngle += 180;
+				} else {
+					// Does nothing.
+				}
 
+			} else {
+				// Does nothing.
 			}
 
 			int AngleDelta = turretAngle - targetAngle;
-			if (Math.abs(AngleDelta) < 3 && clearToShoot && distance < 2.25)
+			
+			if (Math.abs(AngleDelta) < 3 && clearToShoot && distance < 2.25) {
 				firing = true;
+			}
 
 			if (AngleDelta > 0) {
-				if (AngleDelta < 180)
+				if (AngleDelta < 180) {
 					aimRight = true;
-				else
+				} else {
 					aimLeft = true;
+				}
 			} else if (AngleDelta < 0) {
-				if (AngleDelta > -180)
+				if (AngleDelta > -180) {
 					aimLeft = true;
-				else
+				} else {
 					aimRight = true;
+				}
 			}
 
 		}
 	}
+	
+	private void makeTurret() {
+		iDirection = new Vector(0.7, 0, 0);
+		jDirection = new Vector(0, 0.7, 0);
+		kDirection = new Vector(0, 0, 0.7);
 
-	public void draw() {
+		iDirection.rotate_XZ(turretAngle);
+		kDirection.rotate_XZ(turretAngle);
+
+		iDirection.rotate_XZ(180);
+		kDirection.rotate_XZ(180);
+
+		turret = new Polygon3D[22];
+
+		Vector v[];
+
+		v = new Vector[] { put(-0.04, 0.16, -0.08), put(0.04, 0.16, -0.08),
+				put(0.04, 0.09, -0.08), put(-0.04, 0.09, -0.08) };
+		turret[0] = new Polygon3D(v, v[0], v[1], v[3], Main.textures[51], 1,
+				0.5, 6);
+
+		v = new Vector[] { put(-0.08, 0.16, 0.08), put(-0.04, 0.16, -0.08),
+				put(-0.04, 0.09, -0.08), put(-0.08, 0.09, 0.08) };
+		turret[1] = new Polygon3D(v, v[0], v[1], v[3], Main.textures[51], 1,
+				0.5, 6);
+
+		v = new Vector[] { put(0.08, 0.16, 0.08), put(-0.08, 0.16, 0.08),
+				put(-0.08, 0.09, 0.08), put(0.08, 0.09, 0.08) };
+		turret[2] = new Polygon3D(v, v[0], v[1], v[3], Main.textures[51], 1,
+				0.5, 6);
+
+		v = new Vector[] { put(0.04, 0.16, -0.08), put(0.08, 0.16, 0.08),
+				put(0.08, 0.09, 0.08), put(0.04, 0.09, -0.08) };
+		turret[3] = new Polygon3D(v, v[0], v[1], v[3], Main.textures[51], 1,
+				0.5, 6);
+
+		double r = 0.016;
+		double r1 = 0.012;
+		double theta = Math.PI / 8;
+		start.add(0, 0.1, 0);
+
+		for (int i = 0; i < 16; i++) {
+			v = new Vector[] {
+					put(r1 * Math.cos(i * theta), r1 * Math.sin(i * theta),
+							-0.22),
+					put(r1 * Math.cos((i + 1) * theta),
+							r1 * Math.sin((i + 1) * theta), -0.22),
+					put(r * Math.cos((i + 1) * theta),
+							r * Math.sin((i + 1) * theta), -0.08),
+					put(r * Math.cos(i * theta), r * Math.sin(i * theta), -0.08) };
+			turret[i + 4] = new Polygon3D(v, v[0], v[1], v[3],
+					Main.textures[23], 0.001, 0.01, 6);
+			turret[i + 4].Ambient_I = 18;
+			turret[i + 4].diffuse_coefficient = 0.7;
+			turret[i + 4].findDiffuse();
+		}
+
+		v = new Vector[16];
+		
+		for (int i = 1; i < 17; i++) {
+			v[16 - i] = put(r1 * Math.cos(i * theta), r1 * Math.sin(i * theta),
+					-0.22);
+		}
+		
+		turret[21] = new Polygon3D(v, v[0], v[1], v[3], Main.textures[23], 1,
+				1, 6);
+
+		start.add(0, -0.1, 0);
+
+		v = new Vector[] { put(-0.08, 0.16, 0.08), put(0.08, 0.16, 0.08),
+				put(0.04, 0.16, -0.08), put(-0.04, 0.16, -0.08) };
+		turret[20] = new Polygon3D(v, v[0], v[1], put(-0.08, 0.16, -0.08),
+				Main.textures[51], 1, 1, 6);
+
+		start.add(-0.05, 0, -0.05);
+		start.y = -1;
+		v = new Vector[] { put(0.5, 0, -0.27), put(-0.5, 0, -0.27),
+				put(-0.5, 0, 0.27), put(0.5, 0, 0.27) };
+		shadowTurret = new Polygon3D(v, v[0], v[1], v[3], Main.textures[15], 1,
+				1, 2);
+		start.add(0.05, 0, 0.05);
+
+		iDirection.rotate_XZ(180);
+		kDirection.rotate_XZ(180);
+
+	}
+
+	private void makeBody() {
+		Vector v[];
+
+		body = new Polygon3D[5];
+
+		v = new Vector[] { put(-0.09, 0.09, -0.09), put(0.09, 0.09, -0.09),
+				put(0.12, 0, -0.12), put(-0.12, 0, -0.12) };
+		body[0] = new Polygon3D(v, v[0], v[1], v[3], Main.textures[41], 1, 1, 6);
+
+		v = new Vector[] { put(-0.09, 0.09, 0.09), put(-0.09, 0.09, -0.09),
+				put(-0.12, 0, -0.12), put(-0.12, 0, 0.12) };
+		body[1] = new Polygon3D(v, v[0], v[1], v[3], Main.textures[41], 1, 1, 6);
+
+		v = new Vector[] { put(0.12, 0, 0.12), put(0.12, 0, -0.12),
+				put(0.09, 0.09, -0.09), put(0.09, 0.09, 0.09) };
+		body[2] = new Polygon3D(v, v[0], v[1], v[3], Main.textures[41], 1, 1, 6);
+
+		v = new Vector[] { put(-0.12, 0, 0.12), put(0.12, 0, 0.12),
+				put(0.09, 0.09, 0.09), put(-0.09, 0.09, 0.09) };
+		body[3] = new Polygon3D(v, v[0], v[1], v[3], Main.textures[41], 1, 1, 6);
+
+		v = new Vector[] { put(-0.09, 0.09, 0.09), put(0.09, 0.09, 0.09),
+				put(0.09, 0.09, -0.09), put(-0.09, 0.09, -0.09) };
+		body[4] = new Polygon3D(v, v[0], v[1], v[3], Main.textures[41], 1, 1, 6);
+
+		tempVector1 = start.myClone();
+
+		iDirection.scale(1.2);
+		start.add(0, 0, -0.03);
+		v = new Vector[] { put(-0.2, 0, 0.2), put(0.2, 0, 0.2),
+				put(0.2, 0, -0.2), put(-0.2, 0, -0.2) };
+		shadowBody = new Polygon3D(v, v[0], v[1], v[3], Main.textures[50], 1,
+				1, 2);
 
 		for (int i = 0; i < body.length; i++) {
-			body[i].draw();
+			body[i].Ambient_I = 22;
+			body[i].diffuse_coefficient = 0.7;
+			body[i].findDiffuse();
 
 		}
 
-		if (HP > 0) {
-			for (int i = 0; i < turret.length; i++) {
-				turret[i].draw();
-			}
-		}
-
-		if (Smoke != null && visible)
-			Smoke.draw();
+		start.set(tempVector1);
+		iDirection.scale(0.8333333333);
 
 	}
 
-	public Rectangle2D getBoundary2D() {
-		return boundary2D;
-	}
-
-	public void damage(int damagePoint) {
-		HP -= damagePoint;
-	}
 }
