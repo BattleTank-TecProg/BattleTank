963bba68c60016364aa0c9161632c28563955857
Nicacioneto <nicacionetobsb@hotmail.com>
2016-05-15 21:43:30 -0300

    Reduce complexity, add JavaDoc comments, organize asserts

--- Explosion.java
+++ Explosion.java
@@ -20,7 +20,183 @@
  * the game is played.
  */
 public class Explosion extends SolidObject {
+
+	/**
+	 * 
+	 * This is a constructor method. X, y and z are coordinates for the
+	 * explosion and the size is the diameter of explosion.
+	 * 
+	 * @param x
+	 * @param y
+	 * @param z
+	 * @param size
+	 */
+	public Explosion(double x, double y, double z, double size) {
+
+		assert (x > 0 );
+		assert (y < 0);
+		assert (z > 0);
+		assert (size > 0);
+		assert (xDirection == null);
+		assert (yDirection == null);
+		assert (zDirection == null);
+
+		final double LENGHT = 0.001;
+
+		final double HEIGHT = 0.001;
+
+		final double WIDTH = 0.001;
+
+		final double XPOSITION = x - 0.1;
+
+		final double YPOSITION = z + 0.1;
+
+		final double LENGHTRECTANGLE = 0.2;
+
+		final double HEIGHTRECTANGLE = 0.2;
+
+		start = new Vector(x, y, z);
+		xDirection = new Vector(1, 0, 0);
+		yDirection = new Vector(0, 1, 0);
+		zDirection = new Vector(0, 0, 1);
+
+		int random = GameData.getRandom();
+		if (random >= 75) {
+			spriteIndex = 17;
+		} else if (random >= 50) {
+			spriteIndex = 18;
+		} else if (random >= 25) {
+			spriteIndex = 19;
+		} else {
+			spriteIndex = 20;
+		}
+		Vector v[] = new Vector[] { put(-0.3, 0, 0.3), put(0.3, 0, 0.3),
+				put(0.3, 0, -0.3), put(-0.3, 0, -0.3) };
+
+		if (size > 1) {
+			spriteIndex = 18;
+		} else if (size > 3) {
+			v = new Vector[] { put(-0.12, 0, 0.12), put(0.12, 0, 0.12),
+					put(0.12, 0, -0.12), put(-0.12, 0, -0.12) };
+		}
+
+		explosionAura = new Polygon3D(v, v[0], v[1], v[3], Main.textures[21],
+				1, 1, 2);
+
+		this.size = size;
+		/*
+		 * Boundary of this model has a cubic shape (ie length ~= width ~=
+		 * height)
+		 */
+		modelType = 4;
+		makeBoundary(LENGHT, HEIGHT, WIDTH);
+		//Create 2D boundary
+		boundary2D = new Rectangle2D(XPOSITION, YPOSITION, LENGHTRECTANGLE,
+				HEIGHTRECTANGLE);
+
+		lifeSpan = 16;
+		//Find centre of the model in world coordinate
+		findCentre();
+		//Find centre of explosion
+		groundZero = (int) (x * 4) + (129 - (int) (z * 4)) * 80;
+	}
+
+	/**
+	 * This method is responsible for updating the explosion in the scenario,
+	 * according to the actions in the game.
+	 */
+	public void update() {
+		assert (explosionAura != null);
+		
+		visible = true;
+		explosionAura.update();
+		if (explosionAura.visible) {
+			explosionAura.myTexture.Texture = explosionAura.myTexture.lightMapData[auraIndex];
+			Rasterizer.rasterize(explosionAura);
+		} else {
+				// Does nothing
+		}
 	
+		auraIndex++;
+
+		if (!explicitDrawing) {
+			ModelDrawList.register(this);
+		} else {
+			// Does nothing
+		}
+		tempCentre.set(centre);
+		tempCentre.subtract(Camera.getPosition());
+		tempCentre.rotate_XZ(Camera.getXZ_angle());
+		tempCentre.rotate_YZ(Camera.getYZ_angle());
+
+		if (lifeSpan == 15) {
+			ObstacleMap.damageType2Obstacles(damage, boundary2D, groundZero);
+		} else {
+			// Does nothing
+		}
+
+		lifeSpan--;
+
+		if (lifeSpan == 0) {
+			lifeSpan = -1;
+
+		} else {
+			// Does nothing
+		}
+	}
+
+	/** This method is responsible for draw the explosion on the scenario. */
+	public synchronized void draw() {
+
+		tempCentre.updateLocation();
+		/* This variable holds the proportion of explosion */
+		double ratio = size * 2 / tempCentre.z;
+
+		Rasterizer.temp = this.type;
+		Rasterizer.renderExplosionSprite(
+				Main.textures[spriteIndex].explosions[frameIndex], ratio,
+				tempCentre.screenX, tempCentre.screenY, 64, 64);
+
+		frameIndex++;
+
+	}
+
+	/**
+	 * This method is responsible for return the limits of the explosion.
+	 * 
+	 * @return boundary2D
+	 */
+	public Rectangle2D getBoundary2D() {
+		assert (boundary2D != null);
+		return boundary2D;
+	}
+	
+	/** Size of the explosion.*/
+	private double size = 0;
+
+	private int spriteIndex = 0;
+	
+	/** The square indicate the index.*/
+	private int frameIndex = 0;
+	
+	/** Defines the size of the aura.*/
+	private int auraIndex = 0;
+	
+	/** Amount of damage he can tear the opponent.*/
+	private int damage = 5;
+	
+	/** Type of explosion 0 = normal and 1 = plasma.*/
+	private int type = 0;
+	
+	/** Indicates the starting point of explosion.*/
+	private int groundZero = 0;
+	
+	/** Enable this boolean if this explosion has be to drawn explicitly*/
+	private boolean explicitDrawing;
+	
+	/** Define the aura explosion*/
+	private Polygon3D explosionAura;
+
 	public double getSize() {
 		return size;
 	}
@@ -92,171 +268,4 @@ public class Explosion extends SolidObject {
 	public void setExplosionAura(Polygon3D explosionAura) {
 		this.explosionAura = explosionAura;
 	}
-
-	/**
-	 * 
-	 * This is a constructor method. X, y and z are coordinates for the
-	 * explosion and the size is the diameter of explosion.
-	 * 
-	 * @param x
-	 * @param y
-	 * @param z
-	 * @param size
-	 */
-	public Explosion(double x, double y, double z, double size) {
-
-		assert (x > 0 || y < 0 || z > 0 || size > 0);
-		assert (xDirection == null);
-		assert (yDirection == null);
-		assert (zDirection == null);
-
-		final double LENGHT = 0.001;
-
-		final double HEIGHT = 0.001;
-
-		final double WIDTH = 0.001;
-
-		final double XPOSITION = x - 0.1;
-
-		final double YPOSITION = z + 0.1;
-
-		final double LENGHTRECTANGLE = 0.2;
-
-		final double HEIGHTRECTANGLE = 0.2;
-
-		start = new Vector(x, y, z);
-		xDirection = new Vector(1, 0, 0);
-		yDirection = new Vector(0, 1, 0);
-		zDirection = new Vector(0, 0, 1);
-
-		int random = GameData.getRandom();
-		if (random >= 75) {
-			spriteIndex = 17;
-		} else if (random >= 50) {
-			spriteIndex = 18;
-		} else if (random >= 25) {
-			spriteIndex = 19;
-		} else {
-			spriteIndex = 20;
-		}
-		if (size > 1) {
-			spriteIndex = 18;
-		} else {
-			// Does nothing
-		}
-		Vector v[] = new Vector[] { put(-0.3, 0, 0.3), put(0.3, 0, 0.3),
-				put(0.3, 0, -0.3), put(-0.3, 0, -0.3) };
-		if (size > 3) {
-			v = new Vector[] { put(-0.12, 0, 0.12), put(0.12, 0, 0.12),
-					put(0.12, 0, -0.12), put(-0.12, 0, -0.12) };
-		} else {
-			// Does nothing
-		}
-		explosionAura = new Polygon3D(v, v[0], v[1], v[3], Main.textures[21],
-				1, 1, 2);
-
-		this.size = size;
-
-		modelType = 4;
-		makeBoundary(LENGHT, HEIGHT, WIDTH);
-
-		boundary2D = new Rectangle2D(XPOSITION, YPOSITION, LENGHTRECTANGLE,
-				HEIGHTRECTANGLE);
-
-		lifeSpan = 16;
-
-		findCentre();
-
-		groundZero = (int) (x * 4) + (129 - (int) (z * 4)) * 80;
-	}
-
-	/**
-	 * This method is responsible for updating the explosion in the scenario,
-	 * according to the actions in the game.
-	 */
-	public void update() {
-		assert (explosionAura != null);
-		visible = true;
-
-		if (damage != 0) {
-			explosionAura.update();
-			if (explosionAura.visible) {
-				explosionAura.myTexture.Texture = explosionAura.myTexture.lightMapData[auraIndex];
-				Rasterizer.rasterize(explosionAura);
-			} else {
-				// Does nothing
-			}
-		} else {
-			// Does nothing
-		}
-		auraIndex++;
-
-		if (!explicitDrawing) {
-			ModelDrawList.register(this);
-		} else {
-			// Does nothing
-		}
-		tempCentre.set(centre);
-		tempCentre.subtract(Camera.getPosition());
-		tempCentre.rotate_XZ(Camera.getXZ_angle());
-		tempCentre.rotate_YZ(Camera.getYZ_angle());
-
-		if (lifeSpan == 15 && damage != 0) {
-			ObstacleMap.damageType2Obstacles(damage, boundary2D, groundZero);
-		} else {
-			// Does nothing
-		}
-
-		lifeSpan--;
-
-		if (lifeSpan == 0) {
-			lifeSpan = -1;
-
-		} else {
-			// Does nothing
-		}
-	}
-
-	/** This method is responsible for draw the explosion on the scenario. */
-	public synchronized void draw() {
-
-		tempCentre.updateLocation();
-		/* This variable holds the proportion of explosion */
-		double ratio = size * 2 / tempCentre.z;
-
-		Rasterizer.temp = this.type;
-		Rasterizer.renderExplosionSprite(
-				Main.textures[spriteIndex].explosions[frameIndex], ratio,
-				tempCentre.screenX, tempCentre.screenY, 64, 64);
-
-		frameIndex++;
-
-	}
-
-	/** This method is responsible for return the limits of the explosion.
-	 *
-	 * @return boundary2D
-	 */
-	public Rectangle2D getBoundary2D() {
-		assert (boundary2D != null);
-		return boundary2D;
-	}
-
-	private double size = 0;
-
-	private int spriteIndex = 0;
-
-	private int frameIndex = 0;
-
-	private int auraIndex = 0;
-
-	private int damage = 5;
-
-	private int type = 0;
-
-	private int groundZero = 0;
-
-	private boolean explicitDrawing;
-
-	private Polygon3D explosionAura;
 }
