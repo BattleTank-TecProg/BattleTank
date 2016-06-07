de1085d3315694b25d76eb9147d14a76882daeb5
nicacioneto <nicacionetobsb@hotmail.com>
2016-06-07 13:36:37 -0300

    Add some tests

--- CameraTest.java
+++ CameraTest.java
@@ -7,10 +7,16 @@ public class CameraTest {
 	private static final int XCOORDINADE = 0;
 
 	private static final int YCOORDINADE = 0;
-
+	
+	private static int XZ_angle = 0;
+	
+	private static int YZ_angle = 319;
+	
 	private final static int WIDTH = 640;
 
 	private final static int HEIGHT = 480;
+	
+
 	
 	@Test
 	public void testGetXcoordinade() {
@@ -30,7 +36,27 @@ public class CameraTest {
 	@Test
 	public void testGetHeight() {
 		assertEquals(Camera.getHeight(), HEIGHT);
-
+		
 	}
+	
+	
+	@Test
+	public void testGetXZ_angle() {
+		
+		assertEquals(Camera.getXZ_angle(), XZ_angle);
+		
+	}
+
+	
+	@Test
+	public void testGetYZ_angle() {
+		assertEquals(319, YZ_angle);
+	}
+
+	
+
+	
+	
+	
 
 }
