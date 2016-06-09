6b7134a0fd78378d24a6eb2adb5d336da7028162
ruanHerculano <ruannawe95@gmail.com>
2016-06-09 00:21:16 -0300

    Implementing tests in class Explosion

--- ExplosionTest.java
+++ ExplosionTest.java
@@ -3,20 +3,39 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class ExplosionTest {
+	
 	int varibleForComparateResult = 1;
 	Explosion explosion = new Explosion(varibleForComparateResult, varibleForComparateResult, varibleForComparateResult,
 			varibleForComparateResult);
 
-	@Test
-	public void testGetSpriteIndex() {
-		explosion.setSpriteIndex(varibleForComparateResult);
-		assertEquals(explosion.getSpriteIndex(), varibleForComparateResult);
-	}
 	@Test
 	public void testGetAuraIndex() {
 		explosion.setAuraIndex(varibleForComparateResult);
 		assertEquals(explosion.getAuraIndex(), varibleForComparateResult);
 	}
 	
+	@Test
+	public void testGetDamage() {
+		explosion.setDamage(varibleForComparateResult);
+		assertEquals(explosion.getDamage(), varibleForComparateResult);
+	}
+	
+	@Test
+	public void testGetFrameIndex() {
+		explosion.setFrameIndex(varibleForComparateResult);
+		assertEquals(explosion.getFrameIndex(), varibleForComparateResult);
+	}
+	
+	@Test
+	public void testGetGroundZero() {
+		explosion.setGroundZero(varibleForComparateResult);
+		assertEquals(explosion.getGroundZero(), varibleForComparateResult);
+	}
+	
+	@Test
+	public void testGetSpriteIndex() {
+		explosion.setSpriteIndex(varibleForComparateResult);
+		assertEquals(explosion.getSpriteIndex(), varibleForComparateResult);
+	}
 	
 }
