e0e446959f310336b5533714a4cb994594611d61
nicacioneto <nicacionetobsb@hotmail.com>
2016-06-30 21:25:42 -0300

    Add test case for ModelDrawList

---
+++ test/ModelDrawListTest.java
@@ -0,0 +1,37 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import project.Model;
+import project.ModelDrawList;
+
+public class ModelDrawListTest {
+
+	@Test
+	public void testMakeList() {
+		Model[] visibleModels1;
+		ModelDrawList.makeList();
+		visibleModels1 = new Model[300];
+		ModelDrawList.visibleModels = new Model[300];
+		assertArrayEquals(ModelDrawList.visibleModels, visibleModels1);
+		assertNotNull(ModelDrawList.visibleModels);
+		
+	}
+
+	@Test
+	public void testRegister() {
+	}
+
+	@Test
+	public void testDraw() {
+		ModelDrawList.draw();
+		assertNotNull(ModelDrawList.modelCount);
+	}
+
+	@Test
+	public void testSort() {
+	}
+
+}
